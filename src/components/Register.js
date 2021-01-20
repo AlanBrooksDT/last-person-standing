4f98d2d6c0a70c7bb2e66fde35be58e669eae0ca
JackRandom <andrewglogan1987@gmail.com>
2021-01-20 18:34:49 +0000

    useHistory removed

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { Component } from "react";
 import { FormErrors } from "./FormErrors";
 import "../styles/Register.css";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 class Register extends Component {
